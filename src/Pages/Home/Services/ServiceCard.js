02cb2109ca5889882df7902c726ced3432d19772
Marjan-001 <marjan199721@gmail.com>
2022-11-09 21:29:38 +0600

    servicecard added

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 
-const ServiceCard = () => {
+const ServiceCard = ({ service }) => {
+    const { _id, img, price, title, description } = service;
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src='' alt="Shoes" /></figure>
+        <div className="card card-compact w-96 mx-auto bg-base-100 shadow-xl">
+            <figure><img className='h-80 w-full' src={img} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">k</h2>
-                <p className='text-2xl text-orange-600 font-semibold'>Price: </p>
+                <h2 className="card-title">{title}</h2>
+
+                <p className='text-2xl text-rose-600 font-semibold'>Price:{price} </p>
+                <p className='text-xl text-rose-900 py-0 '>Description:{description.slice(100)} </p>
                 <div className="card-actions justify-end">
                     {/* <Link to={`/checkout/${_id}`}>
                         <button className="btn btn-primary">Checkout</button>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+
     return (
         <div>
             <div className='text-center mb-4'>
@@ -8,13 +17,14 @@ const Services = () => {
                 <h2 className="text-5xl font-semibold py-10  text-rose-600">My Services</h2>
 
             </div>
-            {/* <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div className='mx-auto grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
                     services.map(service => <ServiceCard
                         key={service._id}
                         service={service}
                     ></ServiceCard>)
-                } */}
+                }
+            </div>
 
         </div>
     );
